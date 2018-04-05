e5f95cfd843469b1200e6f32da2e9790b8455048
Alex LaFroscia <alex@lafroscia.com>
2018-04-05 12:55:45 -0700

    Prevent blueprint from erroring on installation

--- packages/ember-cli-testdouble-qunit/blueprints/ember-cli-testdouble-qunit/index.js
+++ packages/ember-cli-testdouble-qunit/blueprints/ember-cli-testdouble-qunit/index.js
@@ -3,6 +3,9 @@
 module.exports = {
   description: 'Perform post-installation setup',
 
+  // Prevents error when running blueprint w/o name argument
+  normalizeEntityName() {},
+
   afterInstall() {
     return this.addPackageToProject('ember-cli-testdouble', '^0.1.3');
   }
